b24004f6cb135f858b9d7f09407e5bd26829a3b4
VanLocNguyen1999 <locnv@selex.vn>
2026-06-29 23:27:20 +0700

    adj

--- ev-sdk/hals/sm_hal_exti.h
+++ ev-sdk/hals/sm_hal_exti.h
@@ -10,15 +10,41 @@
 #include <stdint.h>
 
 typedef void sm_hal_exti_t;
-typedef void (*sm_hal_exti_cb_fn_t)(void*);
-
-
-sm_hal_exti_t* sm_hal_exti_init(const void* _instance);
-
-void sm_hal_exti_deinit(sm_hal_exti_t *_this);
-
+typedef void (*sm_hal_exti_cb_fn_t)(uint32_t,void*);
+/**
+ *
+ * @param _instance
+ * @return
+ */
+sm_hal_exti_t* sm_hal_exti_init(const void* _instance, uint32_t _pin);
+/**
+ *
+ * @param _this
+ */
+void sm_hal_pwm_deinit(sm_hal_exti_t *_this);
+/**
+ *
+ * @param _this
+ * @return
+ */
 int32_t sm_hal_exti_start(sm_hal_exti_t *_this);
-
+/**
+ *
+ * @param _this
+ * @return
+ */
 int32_t sm_hal_exti_stop(sm_hal_exti_t *_this);
+/**
+ *
+ * @param _this
+ * @param _cb
+ * @param _arg
+ */
+void sm_hal_exti_set_callback(sm_hal_exti_t *_this, sm_hal_exti_cb_fn_t _cb, void *_arg);
+/**
+ *
+ * @param _this
+ */
+void sm_hal_exti_irq(sm_hal_exti_t *_this);
 
 #endif /* SM_HAL_EXTI_H_ */
--- ev-sdk/platforms/ra4m2_mcu/hal/ra_hal_exti.c
+++ ev-sdk/platforms/ra4m2_mcu/hal/ra_hal_exti.c
@@ -10,21 +10,28 @@
 #define _impl(x)			((sm_hal_flash_impl_t*)(x))
 typedef struct{
 	const external_irq_instance_t* m_instance;
+	uint32_t m_pin;
 	sm_hal_exti_cb_fn_t m_cb;
-	void* arg;
+	void* m_arg;
 }sm_hal_flash_impl_t;
 
-sm_hal_exti_t* sm_hal_exti_init(const void* _instance){
+sm_hal_exti_t* sm_hal_exti_init(const void* _instance, uint32_t _pin){
 
 	if(!_instance) return NULL;
 	sm_hal_flash_impl_t* this = (sm_hal_flash_impl_t*)malloc(sizeof(sm_hal_flash_impl_t));
 	this->m_instance = _instance;
+	this->m_pin = _pin;
 	this->m_cb = NULL;
-	this->arg = NULL;
+	this->m_arg = NULL;
 	return (sm_hal_exti_t*)this;
 }
 
-void sm_hal_pwm_deinit(sm_hal_exti_t *_this);
+void sm_hal_pwm_deinit(sm_hal_exti_t *_this){
+
+    if(!_this)
+        return;
+    free(_this);
+}
 
 int32_t sm_hal_exti_start(sm_hal_exti_t *_this){
 
@@ -48,3 +55,17 @@ int32_t sm_hal_exti_stop(sm_hal_exti_t *_this){
 	return 0;
 }
 
+void sm_hal_exti_set_callback(sm_hal_exti_t *_this, sm_hal_exti_cb_fn_t _cb, void *_arg){
+    if (!_this)
+        return;
+    _impl(_this)->m_cb = _cb;
+    _impl(_this)->m_arg = _arg;
+}
+
+void sm_hal_exti_irq(sm_hal_exti_t *_this) {
+	if (_this && _impl(_this)->m_cb) {
+		bsp_io_level_t level;
+		R_IOPORT_PinRead(&g_ioport_ctrl, _impl(_this)->m_pin, &level);
+		_impl(_this)->m_cb(level, _impl(_this)->m_arg);
+	}
+}
